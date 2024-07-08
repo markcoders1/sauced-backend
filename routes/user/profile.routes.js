7f1a4c51842bbf81c8aa0799da802f49af8cbdd8
Shahzaibalikhawaja <shahzaib.ali.khawaja@gmail.com>
2024-07-09 02:09:45 +0500

    router bug fix

--- routes/user/profile.routes.js
+++ routes/user/profile.routes.js
@@ -9,11 +9,11 @@ const authMiddleware = require("../../middlewares/auth.middleware.js");
 const { upload } = require("../../middlewares/multer.middleware.js");
 
 const router = express.Router();
-router.use(authMiddleware); // Apply auth middleware to all routes in this file
+// router.use(authMiddleware); // Apply auth middleware to all routes in this file
 
-router.patch("/change-name", changeName);
-router.post("/change-image", upload.single("image"), changeImage);
-router.patch("/delete-user", deleteUser);
-router.post("/add-sauce", addSauce);
+router.patch("/change-name", authMiddleware, changeName);
+router.post("/change-image",authMiddleware, upload.single("image"), changeImage);
+router.patch("/delete-user", authMiddleware, deleteUser);
+router.post("/add-sauce", authMiddleware, addSauce);
 
 module.exports = router;
