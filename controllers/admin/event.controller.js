545189a9c0fc810368fb59bf124514e29aad386c
Shahzaibalikhawaja <shahzaib.ali.khawaja@gmail.com>
2024-07-30 01:39:55 +0500

    Add CRUD APIs for event

--- controllers/admin/event.controller.js
+++ controllers/admin/event.controller.js
@@ -48,6 +48,110 @@ const addEvent = async (req, res) => {
 	}
 };
 
+const deleteEvent = async (req, res) => {
+	try {
+		if (req.user.type !== "admin") {
+			return res.status(401).send({ message: "Access denied." });
+		}
+
+		const { eventId } = req.body;
+
+		if (!eventId) {
+			return res.status(400).json({ message: "Event ID is required." });
+		}
+
+		const event = await Event.findById(eventId);
+
+		if (!event) {
+			return res.status(404).json({ message: "Event not found." });
+		}
+
+		await Event.findByIdAndDelete(eventId);
+
+		return res.status(200).json({ message: "Event deleted successfully." });
+	} catch (error) {
+		console.log(error);
+		return res.status(400).json({
+			message: "Something went wrong while deleting the event.",
+		});
+	}
+};
+
+const getEvent = async (req, res) => {
+	try {
+		const { eventId } = req.body;
+		const event = await Event.findById(eventId).populate("owner", "name");
+		if (!event) {
+			return res.status(404).json({ message: "Event not found." });
+		}
+		return res.status(200).json({ event });
+	} catch (error) {
+		console.log(error);
+		return res.status(400).json({
+			message: "Something went wrong while fetching the event.",
+		});
+	}
+};
+
+const getAllEvents = async (req, res) => {
+	try {
+		const events = await Event.find().populate("owner", "name");
+		return res.status(200).json({ events });
+	} catch (error) {
+		console.log(error);
+		return res
+			.status(400)
+			.json({ message: "Something went wrong while fetching events." });
+	}
+};
+
+const updateEvent = async (req, res) => {
+	try {
+		if (req.user.type !== "admin") {
+			return res.status(401).send({ message: "Access denied." });
+		}
+		const {
+			eventId,
+			eventName,
+			eventDetails,
+			eventDate,
+			venueName,
+			venueDescription,
+			venueLocation,
+		} = req.body;
+
+		if (!eventId) {
+			return res.status(400).json({ message: "Event ID is required." });
+		}
+
+		const event = await Event.findById(eventId);
+		if (!event) {
+			return res.status(404).json({ message: "Event not found." });
+		}
+		// Update the event fields
+		event.eventName = eventName || event.eventName;
+		event.eventDetails = eventDetails || event.eventDetails;
+		event.eventDate = eventDate || event.eventDate;
+		event.venueName = venueName || event.venueName;
+		event.venueDescription = venueDescription || event.venueDescription;
+		event.venueLocation = venueLocation || event.venueLocation;
+
+		await event.save();
+		return res
+			.status(200)
+			.json({ message: "Event updated successfully", event });
+	} catch (error) {
+		console.log(error);
+		return res.status(400).json({
+			message: "Something went wrong while updating the event.",
+		});
+	}
+};
+
 module.exports = {
 	addEvent,
+	deleteEvent,
+	getEvent,
+	getAllEvents,
+	updateEvent,
 };
--- routes/admin/event.routes.js
+++ routes/admin/event.routes.js
@@ -1,5 +1,11 @@
 const express = require("express");
-const { addEvent } = require("../../controllers/admin/event.controller.js");
+const {
+	addEvent,
+	deleteEvent,
+	getEvent,
+	getAllEvents,
+	updateEvent,
+} = require("../../controllers/admin/event.controller.js");
 
 // const adminMiddleware = require("../../middlewares/admin.middleware.js");
 
@@ -7,5 +13,9 @@ const adminRouter = express.Router();
 // adminRouter.use(adminMiddleware);
 
 adminRouter.post("/add-event", addEvent);
+adminRouter.delete("/delete-event", deleteEvent);
+adminRouter.get("/get-event", getEvent);
+adminRouter.get("/get-all-events", getAllEvents);
+adminRouter.put("/update-event", updateEvent);
 
 module.exports = adminRouter;
